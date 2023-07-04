41c82f4f0d54432b75d2acf532825e773cb9b51f
Gaël Reyrol <me@gaelreyrol.dev>
2023-07-04 16:03:34 +0200

    refactor: use direct arguments

--- default.nix
+++ default.nix
@@ -1,19 +1,26 @@
-{ pkgs, ... }:
+{ lib
+, stdenv
+, fetchurl
+, fetchzip
+, makeWrapper
+, unzip
+, util-linux
+}:
 
 let 
-  koreader = pkgs.fetchurl {
+  koreader = fetchurl {
     url = "https://storage.gra.cloud.ovh.net/v1/AUTH_2ac4bfee353948ec8ea7fd1710574097/kfmon-pub/OCP-KOReader-v2023.06.zip";
     hash = "sha256-oB9Naia3mA74nVrgjfK5gudIcgJAM6xoVFOpV6ygfdY=";
   };
-  kfmon = pkgs.fetchurl {
+  kfmon = fetchurl {
     url = "https://storage.gra.cloud.ovh.net/v1/AUTH_2ac4bfee353948ec8ea7fd1710574097/kfmon-pub/OCP-KFMon-1.4.6-19-g490e0bb.zip";
     hash = "sha256-zWSvUUeD/RsKcNwH00wE5FeGmMZhXBcdA+ujpJ5ZwY0=";
   };
-in pkgs.stdenv.mkDerivation (finalAttrs: {
+in stdenv.mkDerivation (finalAttrs: {
   pname = "koreader-installer";
   version = "unstable-2023-06";
 
-  src = pkgs.fetchzip {
+  src = fetchzip {
     url = "https://storage.gra.cloud.ovh.net/v1/AUTH_2ac4bfee353948ec8ea7fd1710574097/kfmon-pub/kfm_nix_install.zip";
     hash = "sha256-5z7EyN0/gKzAQ0u8UKA/YOOea5NQRcGKOudsniw1P+A=";
   };
@@ -21,7 +28,7 @@ in pkgs.stdenv.mkDerivation (finalAttrs: {
   dontConfigure = true;
   dontBuild = true;
 
-  nativeBuildInputs = [ pkgs.makeWrapper ];
+  nativeBuildInputs = [ makeWrapper ];
 
   installPhase = ''
     runHook preInstall
@@ -29,14 +36,14 @@ in pkgs.stdenv.mkDerivation (finalAttrs: {
     mkdir $out
     mv install.sh $out/koreader-installer
     wrapProgram "$out/koreader-installer" \
-      --prefix PATH ":" "${pkgs.lib.makeBinPath [ pkgs.unzip pkgs.util-linux ]}"
+      --prefix PATH ":" "${lib.makeBinPath [ unzip util-linux ]}"
     ln -s ${koreader} $out/OCP-KOReader-${finalAttrs.version}.zip
     ln -s ${kfmon} $out/OCP-KFMon-${finalAttrs.version}.zip
 
     runHook postInstall
   '';
 
-  meta = with pkgs.lib; {
+  meta = with lib; {
     description = "An ebook reader application supporting PDF, DjVu, EPUB, FB2 and many more formats, running on Cervantes, Kindle, Kobo, PocketBook and Android devices";
     homepage = "http://koreader.rocks/";
     changelog = "https://github.com/koreader/koreader/releases/tag/v${finalAttrs.version}";

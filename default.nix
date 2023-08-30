dcaf17e561c130979c359d57c27099904f237665
Gaël Reyrol <me@gaelreyrol.dev>
2023-08-30 09:50:27 +0200

    feat: update KOReader to 2023.08 and KFMon to 1.4.6-28

--- default.nix
+++ default.nix
@@ -7,18 +7,18 @@
 , util-linux
 }:
 
-let 
+let
   koreader = fetchurl {
-    url = "https://storage.gra.cloud.ovh.net/v1/AUTH_2ac4bfee353948ec8ea7fd1710574097/kfmon-pub/OCP-KOReader-v2023.06.zip";
-    hash = "sha256-oB9Naia3mA74nVrgjfK5gudIcgJAM6xoVFOpV6ygfdY=";
+    url = "https://storage.gra.cloud.ovh.net/v1/AUTH_2ac4bfee353948ec8ea7fd1710574097/kfmon-pub/OCP-KOReader-v2023.08.zip";
+    hash = "sha256-f8oUHYzGoujgzxtURqcZSF5And1jTXRK8JHKyrMNxFM=";
   };
   kfmon = fetchurl {
-    url = "https://storage.gra.cloud.ovh.net/v1/AUTH_2ac4bfee353948ec8ea7fd1710574097/kfmon-pub/OCP-KFMon-1.4.6-19-g490e0bb.zip";
-    hash = "sha256-zWSvUUeD/RsKcNwH00wE5FeGmMZhXBcdA+ujpJ5ZwY0=";
+    url = "https://storage.gra.cloud.ovh.net/v1/AUTH_2ac4bfee353948ec8ea7fd1710574097/kfmon-pub/OCP-KFMon-1.4.6-28-g32f4afc.zip";
+    hash = "sha256-bVMTJqnhzole9SfobFkGBGRRWbXIhJbQeTfOB/J6HzA=";
   };
 in stdenv.mkDerivation (finalAttrs: {
   pname = "koreader-installer";
-  version = "unstable-2023-06";
+  version = "2.0.0";
 
   src = fetchzip {
     url = "https://storage.gra.cloud.ovh.net/v1/AUTH_2ac4bfee353948ec8ea7fd1710574097/kfmon-pub/kfm_nix_install.zip";
@@ -37,7 +37,7 @@ in stdenv.mkDerivation (finalAttrs: {
     mv install.sh $out/koreader-installer
     wrapProgram "$out/koreader-installer" \
       --prefix PATH ":" "${lib.makeBinPath [ unzip util-linux ]}"
-    ln -s ${koreader} $out/OCP-KOReader-${finalAttrs.version}.zip
+    ln -s ${koreader} $out/OCP-KOReader-v${finalAttrs.version}.zip
     ln -s ${kfmon} $out/OCP-KFMon-${finalAttrs.version}.zip
 
     runHook postInstall
@@ -46,7 +46,7 @@ in stdenv.mkDerivation (finalAttrs: {
   meta = with lib; {
     description = "An ebook reader application supporting PDF, DjVu, EPUB, FB2 and many more formats, running on Cervantes, Kindle, Kobo, PocketBook and Android devices";
     homepage = "http://koreader.rocks/";
-    changelog = "https://github.com/koreader/koreader/releases/tag/v${finalAttrs.version}";
+    changelog = "https://github.com/gaelreyrol/koreader-nix/releases/tag/v${finalAttrs.version}";
     platforms = platforms.linux;
     license = licenses.agpl3;
     maintainers = with maintainers; [ gaelreyrol ];
